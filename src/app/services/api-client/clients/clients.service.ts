37b6aa247582dbfc91b22004e7a27ade22c4c1b1
duarte-arthur21 <ferreiraduarte21@gmail.com>
2025-04-06 03:08:10 -0300

    feat: implement client service methods for CRUD operations

--- src/app/services/api-client/clients/clients.service.ts
+++ src/app/services/api-client/clients/clients.service.ts
@@ -3,26 +3,28 @@ import { IClientsService } from './iclients.service';
 import { Observable } from 'rxjs';
 import { SaveClientRequest, SaveClientResponse, UpdateClientResquest, UpdateClientResponse, ListClientResponse, DetailsClientResponse } from './client.models';
 import { HttpClient } from '@angular/common/http';
+import { envionment } from '../../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService implements IClientsService {
+  private readonly basePath = envionment.apiUrl; //ou 1080
 
   constructor(private http: HttpClient) { }
-  save(resquest: SaveClientRequest): Observable<SaveClientResponse> {
-    throw new Error('Method not implemented.');
+  save(request: SaveClientRequest): Observable<SaveClientResponse> {
+    return this.http.post<SaveClientResponse>(`${this.basePath}clients`, request);
   }
   update(id: number, request: UpdateClientResquest): Observable<UpdateClientResponse> {
-    throw new Error('Method not implemented.');
+    return this.http.put<UpdateClientResponse>(`${this.basePath}clients/${id}`, request);
   }
   delete(id: number): Observable<void> {
-    throw new Error('Method not implemented.');
+    return this.http.delete<void>(`${this.basePath}clients/${id}`);
   }
   list(): Observable<ListClientResponse[]> {
-    throw new Error('Method not implemented.');
+    return this.http.get<ListClientResponse[]>(`${this.basePath}clients`);
   }
   findByID(id: number): Observable<DetailsClientResponse> {
-    throw new Error('Method not implemented.');
+    return this.http.get<DetailsClientResponse>(`${this.basePath}clients/${id}`);
   }
 }
