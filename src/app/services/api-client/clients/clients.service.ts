059b75068d889b96d23cfef2a82e237abaf3a326
duarte-arthur21 <ferreiraduarte21@gmail.com>
2025-04-06 01:37:04 -0300

    feat: implement ClientsService class with IClientsService methods

--- src/app/services/api-client/clients/clients.service.ts
+++ src/app/services/api-client/clients/clients.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { IClientsService } from './iclients.service';
+import { Observable } from 'rxjs';
+import { SaveClientRequest, SaveClientResponse, UpdateClientResquest, UpdateClientResponse, ListClientResponse, DetailsClientResponse } from './client.models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClientsService {
+export class ClientsService implements IClientsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  save(resquest: SaveClientRequest): Observable<SaveClientResponse> {
+    throw new Error('Method not implemented.');
+  }
+  update(id: number, request: UpdateClientResquest): Observable<UpdateClientResponse> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: number): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
+  list(): Observable<ListClientResponse[]> {
+    throw new Error('Method not implemented.');
+  }
+  findByID(id: number): Observable<DetailsClientResponse> {
+    throw new Error('Method not implemented.');
+  }
 }
